d274f5266fa0f6d4f004ca979a03476d023ef040
amiad <makomtovapp@gmail.com>
2018-04-04 13:56:44 +0300

    fix login guard

--- makom-tov/src/app/services/login-route-guard.service.ts
+++ makom-tov/src/app/services/login-route-guard.service.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@angular/core';
-import { FbServiceService } from './fb-service.service';
-import { ModalDialogService } from 'ngx-modal-dialog';
-import { CustomModalComponent } from '../custom-modal/custom-modal.component';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterComponent } from '../register/register.component';
 
 @Injectable()
 export class LoginRouteGuardService implements CanActivate {
 
-  constructor(
-    private router: Router,
-    private modalDialogService: ModalDialogService,
-    private fbService: FbServiceService,
-     private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-      const modalRef = this.modalService.open(RegisterComponent);
-      return false;
-    }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const modalRef = this.modalService.open(RegisterComponent);
+    return false;
   }
+}
